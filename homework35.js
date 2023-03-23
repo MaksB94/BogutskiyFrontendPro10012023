5ba5138fd6bc7ca1d25e8438b66c74326d932e70
MaksB94 <pa280694bmv@gmail.com>
2023-03-23 20:47:58 +0200

    homework35

---
+++ homework35.js
@@ -0,0 +1,12 @@
+const table = document.createElement("table");
+
+for (let i = 0; i < 10; i++) {
+  const row = table.insertRow();
+
+ 
+  for (let j = 0; j < 10; j++) {
+    const cell = row.insertCell();
+    cell.innerText = i * 10 + j + 1;   }
+}
+
+document.body.appendChild(table);
