b4c52293bab904a7f2b44df96d5c47dc5a005e5f
MaksB94 <pa280694bmv@gmail.com>
2023-05-01 19:43:32 +0300

    homework48

---
+++ homework48/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+      <section className="section">
+        <header className="header">Header</header>
+        <div className="container">
+          <nav>Left Navigation</nav>
+          <main>Central Container</main>
+        </div>
+      </section>
+  );
+}
+
+export default App;
